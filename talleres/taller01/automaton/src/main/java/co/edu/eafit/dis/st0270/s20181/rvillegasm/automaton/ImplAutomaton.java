dd644715b3d31098c370852fbbfe8fa9b23a6ebc
rvillegasm <rvillegasm@e42379f6-78d1-4897-8fcc-def896e45baa>
2018-03-24 02:59:30 +0000

    Taller 1 cleaning() y isclean() agregados

--- talleres/taller01/automaton/src/main/java/co/edu/eafit/dis/st0270/s20181/rvillegasm/automaton/ImplAutomaton.java
+++ talleres/taller01/automaton/src/main/java/co/edu/eafit/dis/st0270/s20181/rvillegasm/automaton/ImplAutomaton.java
@@ -2,6 +2,9 @@ package co.edu.eafit.dis.st0270.s20181.rvillegasm.automaton;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
+import java.lang.Character;
+import org.apache.commons.lang3.tuple.Pair;
 import co.edu.eafit.dis.st0270.automaton.State;
 import co.edu.eafit.dis.st0270.automaton.Automaton;
 import co.edu.eafit.dis.st0270.automaton.AutomatonException;
@@ -12,13 +15,13 @@ import co.edu.eafit.dis.st0270.automaton.AutomatonException;
  *
  */
 
-  // TODO: implement cleaning(), getMinimal(), isCleaning(), isMinimal()
+  // TODO: check if you can "return this" in cleaning(), getMinimal(), isMinimal()
 public class ImplAutomaton extends Automaton
 {
     public ImplAutomaton(
                             Set<State> states, 
-                            Set<java.lang.Character> alphabet, 
-                            Map<org.apache.commons.lang3.tuple.Pair<State,java.lang.Character>,State> delta, 
+                            Set<Character> alphabet, 
+                            Map<Pair<State,Character>,State> delta, 
                             State initial, 
                             Set<State> finals
 
@@ -30,7 +33,37 @@ public class ImplAutomaton extends Automaton
     
 	@Override
 	public Automaton cleaning() throws AutomatonException, IllegalArgumentException {
-		return null;
+		//Getting the accesible states
+		Set<State> accesible = new HashSet<State>();
+		for(State q : states) {
+			for(Character a : alphabet) {
+				if(delta.containsKey(Pair.of(initial, a)) && delta.get(Pair.of(initial, a)).equals(q)) {
+					accesible.add(q);
+				}
+			}
+		}
+		//Getting the Post-accesible states
+		Set<State> postAccesible = new HashSet<State>();
+		for(State q : states) {
+			for(Character a : alphabet) {
+				if(delta.containsKey(Pair.of(q, a)) && finals.contains(delta.get(Pair.of(q, a)))) {
+					postAccesible.add(q);
+				}
+			}
+		}
+		//Looking for the obsolete states and removing them from the automaton
+		for(State q : states) {
+			if(!accesible.contains(q) || !postAccesible.contains(q)) {
+				for(Character a : alphabet) {
+					if(delta.containsKey(Pair.of(q, a))) {
+						delta.remove(Pair.of(q, a));
+					}
+				}
+				states.remove(q);
+				finals.remove(q);
+			}
+		}
+		return this;
 	}
 
 	@Override
@@ -40,7 +73,31 @@ public class ImplAutomaton extends Automaton
 
 	@Override
 	public boolean isClean() {
-		return false;
+		//Getting the accesible states
+		Set<State> accesible = new HashSet<State>();
+		for(State q : states) {
+			for(Character a : alphabet) {
+				if(delta.containsKey(Pair.of(initial, a)) && delta.get(Pair.of(initial, a)).equals(q)) {
+					accesible.add(q);
+				}
+			}
+		}
+		//Getting the Post-accesible states
+		Set<State> postAccesible = new HashSet<State>();
+		for(State q : states) {
+			for(Character a : alphabet) {
+				if(delta.containsKey(Pair.of(q, a)) && finals.contains(delta.get(Pair.of(q, a)))) {
+					postAccesible.add(q);
+				}
+			}
+		}
+		//Check if every state is in both sets. If not, it is not clean
+		for(State q : states) {
+			if(!accesible.contains(q) || !postAccesible.contains(q)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
