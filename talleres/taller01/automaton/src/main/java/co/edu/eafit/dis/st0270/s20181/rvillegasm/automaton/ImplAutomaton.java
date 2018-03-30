82b42af1d75dce71e769716925060c2152c7411a
rvillegasm <rvillegasm@e42379f6-78d1-4897-8fcc-def896e45baa>
2018-03-30 19:49:29 +0000

    Funciones auxiliares agregadas

--- talleres/taller01/automaton/src/main/java/co/edu/eafit/dis/st0270/s20181/rvillegasm/automaton/ImplAutomaton.java
+++ talleres/taller01/automaton/src/main/java/co/edu/eafit/dis/st0270/s20181/rvillegasm/automaton/ImplAutomaton.java
@@ -2,6 +2,7 @@ package co.edu.eafit.dis.st0270.s20181.rvillegasm.automaton;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.lang.Character;
 import org.apache.commons.lang3.tuple.Pair;
@@ -15,40 +16,38 @@ import co.edu.eafit.dis.st0270.automaton.AutomatonException;
  *
  */
 
-  // TODO: check if you can "return this" in cleaning(), getMinimal(), isMinimal()
+  // TODO: change every atribute for this.<atribute> in cleaning(), implement DFS with delta (like graphs)
 public class ImplAutomaton extends Automaton
 {
-    public ImplAutomaton(
-                            Set<State> states, 
-                            Set<Character> alphabet, 
-                            Map<Pair<State,Character>,State> delta, 
-                            State initial, 
-                            Set<State> finals
+  public ImplAutomaton(
+                          Set<State> states, 
+                          Set<Character> alphabet, 
+                          Map<Pair<State,Character>,State> delta, 
+                          State initial, 
+                          Set<State> finals
 
-                        ) throws AutomatonException, IllegalArgumentException {
+                      ) throws AutomatonException, IllegalArgumentException {
 
-        super(states, alphabet, delta, initial, finals);
+    super(states, alphabet, delta, initial, finals);
 
-    }
+  }
     
 	@Override
 	public Automaton cleaning() throws AutomatonException, IllegalArgumentException {
 		//Getting the accesible states
 		Set<State> accesible = new HashSet<State>();
 		for(State q : states) {
-			for(Character a : alphabet) {
-				if(delta.containsKey(Pair.of(initial, a)) && delta.get(Pair.of(initial, a)).equals(q)) {
-					accesible.add(q);
-				}
+			Map<State,Boolean> visited = new HashMap<State,Boolean>();
+			if(isAccesible(visited, delta, alphabet, q, initial)) {
+				accesible.add(q);
 			}
 		}
 		//Getting the Post-accesible states
 		Set<State> postAccesible = new HashSet<State>();
 		for(State q : states) {
-			for(Character a : alphabet) {
-				if(delta.containsKey(Pair.of(q, a)) && finals.contains(delta.get(Pair.of(q, a)))) {
-					postAccesible.add(q);
-				}
+			Map<State,Boolean> visited = new HashMap<State,Boolean>();
+			if(isPostAccesible(visited, delta, alphabet, finals, q)) {
+				postAccesible.add(q);
 			}
 		}
 		//Looking for the obsolete states and removing them from the automaton
@@ -64,6 +63,38 @@ public class ImplAutomaton extends Automaton
 			}
 		}
 		return this;
+	}
+	// auxiliar function for accesible states
+	private boolean isAccesible( 
+																Map<State,Boolean> visited, 
+																Map<Pair<State,Character>,State> delta, 
+																Set<Character> alphabet, 
+																State target, 
+																State q
+															) {
+		visited.put(q, true);
+		for(Character a : alphabet) {
+			if((delta.containsKey(Pair.of(q, a)) && !visited.get(delta.get(Pair.of(q, a)))) && (delta.get(Pair.of(q, a)).equals(target) || isAccesible(visited, delta, alphabet, target, delta.get(Pair.of(q, a))))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	// auxiliar function for post-accesible states
+	private boolean isPostAccesible( 
+																Map<State,Boolean> visited, 
+																Map<Pair<State,Character>,State> delta, 
+																Set<Character> alphabet, 
+																Set<State> finals, 
+																State q
+															) {
+		visited.put(q, true);
+		for(Character a : alphabet) {
+			if((delta.containsKey(Pair.of(q, a)) && !visited.get(delta.get(Pair.of(q, a)))) && (finals.contains(delta.get(Pair.of(q, a))) || isPostAccesible(visited, delta, alphabet, finals, delta.get(Pair.of(q, a))))) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
